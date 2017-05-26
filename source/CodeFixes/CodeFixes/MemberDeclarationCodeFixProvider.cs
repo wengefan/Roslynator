f67b9a436b28849cbeab1f700a3a72d61c55e175
Josef Pihrt <josef.pihrt@outlook.com>
2017-05-26 08:28:21 +0200

    Add code fix for CannotChangeAccessModifiersWhenOverridingInheritedMember (CS0507).

---
+++ source/CodeFixes/CodeFixes/MemberDeclarationCodeFixProvider.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Immutable;
+using System.Composition;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Roslynator.CSharp.Refactorings;
+
+namespace Roslynator.CSharp.CodeFixes
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(MemberDeclarationCodeFixProvider))]
+    [Shared]
+    public class MemberDeclarationCodeFixProvider : BaseCodeFixProvider
+    {
+        public sealed override ImmutableArray<string> FixableDiagnosticIds
+        {
+            get { return ImmutableArray.Create(CSharpErrorCodes.CannotChangeAccessModifiersWhenOverridingInheritedMember); }
+        }
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            if (!Settings.IsCodeFixEnabled(CodeFixIdentifiers.OverridingMemberCannotChangeAccessModifiers))
+                return;
+
+            SyntaxNode root = await context.GetSyntaxRootAsync().ConfigureAwait(false);
+
+            MemberDeclarationSyntax memberDeclaration = root
+                .FindNode(context.Span, getInnermostNodeForTie: true)?
+                .FirstAncestorOrSelf<MemberDeclarationSyntax>();
+
+            Debug.Assert(memberDeclaration != null, $"{nameof(memberDeclaration)} is null");
+
+            if (memberDeclaration == null)
+                return;
+
+            foreach (Diagnostic diagnostic in context.Diagnostics)
+            {
+                switch (diagnostic.Id)
+                {
+                    case CSharpErrorCodes.CannotChangeAccessModifiersWhenOverridingInheritedMember:
+                        {
+                            SemanticModel semanticModel = await context.GetSemanticModelAsync().ConfigureAwait(false);
+
+                            OverrideInfo overrideInfo = OverridingMemberCannotChangeAccessModifiersRefactoring.GetOverrideInfo(memberDeclaration, semanticModel, context.CancellationToken);
+
+                            string title = $"Change accessibility to '{overrideInfo.DeclaredAccessibilityText}'";
+
+                            CodeAction codeAction = CodeAction.Create(
+                                title,
+                                cancellationToken => OverridingMemberCannotChangeAccessModifiersRefactoring.RefactorAsync(context.Document, memberDeclaration, overrideInfo, cancellationToken),
+                                diagnostic.Id + EquivalenceKeySuffix);
+
+                            context.RegisterCodeFix(codeAction, diagnostic);
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}
